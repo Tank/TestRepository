4dfe27777c85de0f8870e139bdd0bc99268d166b
Tank <frog2020@126.com>
2013-12-28 21:06:05 +0800

    add unit test

---
+++ GuildBrowserTests/CharacterTests.h
@@ -0,0 +1,13 @@
+//
+//  CharacterTests.h
+//  GuildBrowser
+//
+//  Created by Qtone on 12/28/13.
+//  Copyright (c) 2013 Charlie Fulton. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface CharacterTests : SenTestCase
+
+@end
